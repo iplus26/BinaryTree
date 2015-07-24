4cb9eb950f1b415ea44263c007da4774e8a725ab
iplus26 <iplus26@gmail.com>
2015-07-25 00:13:11 +0800

    完成基本功能和逻辑

--- js/data.js
+++ js/data.js
@@ -1,57 +1,43 @@
+'use strict';
+
 if (!window.localStorage) {
     alert('全球只有7%的浏览器不支持“本地存储”特性，其中包括您。您需要更换更高级的浏览器方可使用我们的应用。');
 }
 
 var storage = window.localStorage;
 
-var table = document.querySelector("table");
-table.innerHTML = "";
+// console.log(storage.data);
 
-console.log(storage);
+var table = document.querySelector('table');
+table.innerHTML = '';
 
 // 如果没有数据, 则制造假数据
-if (storage.data == null || storage.data == "") {
-    var fake = [];
+function fakeData() {
+    if (typeof storage.data === 'undefined') {
+        var fake = [];
+        ( function fakeData() {
+            for (var i = 0; i < 260; i++) {
+                fake.push({
+                    category: 8,
+                    mode: Math.random() > 0.5 ? 1 : -1,
+                    abs: (Math.random() * 1000).toFixed(2), // return String
+                    date: new Date().setDate(22 - Math.random() * 100)
+                });
+            }
+        } )();
 
-    fake.push({
-        category: 0,
-        note: "App Store",
-        type: -1,
-        num: -6,
-        date: new Date()
-    });
-
-    fake.push({
-        category: 1,
-        note: "电脑",
-        type: -1,
-        num: -6000,
-        date: new Date()
-    });
-
-    ( function fakeData() {
-        for (var i = 0; i < 260; i++) {
-            fake.push({
-                category: parseInt(Math.random() * 10),
-                note: "随机假数据",
-                type: Math.random() > 0.5 ? 1 : -1,
-                num: (Math.random() * 1000).toFixed(2),
-                date: new Date().setDate(22 - Math.random() * 100)
-            });
-        }
-    } )();
-
-    storage.data = JSON.stringify(fake);
+        storage.data = JSON.stringify(fake);
+    }
 }
 
 // 补充Date的相关函数，包括格式化日期、判断今天、昨天
 var today = new Date();
-Date.prototype.today = today.getFullYear() + "/" + (today.getMonth() + 1) + "/" + today.getDate();
-Date.prototype.yesterday = today.getFullYear() + "/" + (today.getMonth() + 1) + "/" + (today.getDate() - 1);
+Date.prototype.today = today.getFullYear() + '/' + (today.getMonth() + 1) + '/' + today.getDate();
+Date.prototype.yesterday = today.getFullYear() + '/' + (today.getMonth() + 1) + '/' + (today.getDate() - 1);
 Date.prototype.format = function() {
-    var str = this.getFullYear() + "/" + (this.getMonth() + 1) + "/" + this.getDate();
-    if (str == this.today) return "今天";
-    if (str == this.yesterday) return "昨天";
+    var str = this.getFullYear() + '/' + (this.getMonth() + 1) + '/' + this.getDate();
+    if (str == this.today) return '今天';
+    if (str == this.yesterday) return '昨天';
     return str;
 };
 
@@ -59,33 +45,49 @@ var data;
 
 // 从localStorage中取回数据
 ( function retrieveData() {
+
     data = JSON.parse(storage.data);
+
     for (var i = 0; i < data.length; i++) {
         data[i].date = new Date(data[i].date);
     }
 
-    // Chrome 有 bug
     data.sort(function(a, b) {
         return Date.parse(b.date) - Date.parse(a.date);
     });
+
 } )();
+
+var categories = ['一般', '饮食', '购物', '居家', '手机', '电脑', '社交', '个人', '随机假数据'];
+var category_icons = [
+    '<i class="icono-tag"></i>', '<i class="icono-cup"></i>',
+    '<i class="icono-cart"></i>', '<i class="icono-home"></i>',
+    '<i class="icono-nexus"></i>', '<i class="icono-macbookBold"></i>',
+    '<i class="icono-smile"></i>', '<i class="icono-user"></i>',
+    '<i class="icono-exclamation"></i>'
+];
 
 // 显示数据
-// TODO: 数据较多时显示部分
+// TODO: 数据较多时显示部分, ajax? 
 ( function showData() {
+    var statistics = 0;
     for (var i = 0; i < data.length; i++) {
-        table.innerHTML += ("<tr><td class=\"icon\">" + data[i].category + "</td>") +
-            ("<td class=\"category\">" + data[i].note + "</td>") +
-            ("<td class=\"record " + (data[i].type === -1 ? "cost\">- " : "earn\">+ ") + data[i].num + "</td>") +
-            ("<td class=\"date\">" + data[i].date.format() + "</td></tr>");
+        table.innerHTML += '<tr>' +
+            '<td class="icon">' + category_icons[data[i].category] + '</td>' +
+            '<td class="category">' + categories[data[i].category] + '</td>' +
+            '<td class="record ' + (data[i].mode === -1 ? 'cost">-' : 'earn">+') +
+            data[i].abs + '</td>' +
+            '<td class="date">' + data[i].date.format() + '</td>' +
+            '</tr>';
+        statistics += parseFloat(data[i].abs) * data[i].mode;
     }
+    document.querySelector('div#overview').innerText = statistics.toFixed(2);
 } )();
 
-// 统计代码
-// document.querySelector(".count").innerHTML = document.querySelectorAll(".cost").length + "/" + document.querySelectorAll(".earn").length;
-
 // 清除数据
-// storage.removeItem("data");
+function clearStorage() {
+    storage.removeItem('data');
+}
 
 // 添加新的记录
 function addRecord() {
@@ -93,5 +95,8 @@ function addRecord() {
 }
 
 function more() {
-    window.location.href = 'index.html#buttom';
+    // window.location.href = 'index.html#buttom';
+    if (confirm('开发模式：清除全部数据! ')) {
+        clearStorage();
+    }
 }
--- js/save.js
+++ js/save.js
@@ -1,7 +1,58 @@
+'use strict';
+
 function goBack() {
     window.location.href = 'index.html';
 }
 
-function saveRecord() {
-    goBack();
+function changeMode() {
+    document.querySelector('#title').innerText = mode === -1 ? 'EARN' : 'COST';
+    mode *= -1;
 }
+
+function saveRecord() {
+    var val = document.querySelector('input[type="number"]').value;
+
+    if (/[0-9]*(.)*[0-9]*/.exec(val) && val !== '0') {
+        var abs = Math.abs(val);
+        var storage = window.localStorage;
+        var data = [];
+
+        if (typeof storage.data !== 'undefined') {
+            data = JSON.parse(storage.data);
+        }
+
+        var record = {
+            category: category,
+            mode: mode,
+            abs: abs.toFixed(2),
+            date: new Date()
+        };
+        data.push(record);
+
+        data.sort(function(a, b) {
+            return Date.parse(b.date) - Date.parse(a.date);
+        });
+        // console.log(data);
+        storage.data = JSON.stringify(data);
+
+        if (confirm('成功添加一条开销！')) {
+            goBack();
+        }
+    } else {
+        alert('请输入一个符合规范的数，可以有小数点，但不要有横线空格等等。');
+    }
+}
+
+function chooseCategory(index) {
+    document.querySelector('.chosen').classList.remove('chosen');
+    document.querySelector('.category.' + categories[index]).classList.add('chosen');
+    category = index;
+}
+
+var category = 0,
+    mode = -1,
+    abs = 0;
+var categories = [
+    'normal', 'cup', 'cart', 'home',
+    'cellphone', 'macbook', 'social', 'user'
+];
